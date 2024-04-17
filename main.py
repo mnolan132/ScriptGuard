9cc8e2107a4474dd8f7839d0bc692b66483d4a5a
Matthew Nolan <mnolan132@gmail.com>
2024-04-18 09:12:51 +1200

    UI improvements

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import extract_links.extract as extract
 import xss_scan.xss_scan as xss
 import sql_injection_scan.sql_scan as sql
 
-internal_urls = extract.crawl("https://www.thepythoncode.com", 30)
+# internal_urls = extract.crawl("https://www.thepythoncode.com", 30)
 
-for url in internal_urls:
-    print(xss.scan_xss(url))
-    print(sql.scan_sql_injection(url))
+# for url in internal_urls:
+#     print(xss.scan_xss(url))
+#     print(sql.scan_sql_injection(url))
 
 
+xss.scan_xss("https://alf.nu/alert1?world=alert&level=alert0")
+sql.scan_sql_injection("https://alf.nu/alert1?world=alert&level=alert0")
