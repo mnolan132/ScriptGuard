da74a6eec1a1eeec07ae2e04abc7090e1740951a
Matthew Nolan <mnolan132@gmail.com>
2024-05-04 13:25:14 +1200

    begin building server functionality

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from config import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_developer = db.Column(db.Bool, nullable=False)
+    darkmode_default = db.Column(db.Bool, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+            "password": self.password,
+            "isDeveloper": self.is_developer,
+            "darkmodeDefault": self.darkmode_default
+        }
